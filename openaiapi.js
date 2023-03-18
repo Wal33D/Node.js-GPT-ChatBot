420d45231cc6ab0505cbd015e51b74a4ffdf7bcc
unknown <aquataze@yahoo.com>
2023-03-18 08:52:59 -0400

    First commit

--- openaiapi.js
+++ openaiapi.js
@@ -1,9 +1,11 @@
+if (process.env.OPENAI_API_KEY) {
+console.log("Your OpenAI API key is:", process.env.OPENAI_API_KEY);
+} else {
+console.log("Please get and set your OpenAI API key. Here's the link to obtain it: https://platform.openai.com/account/api–keys");
+}
+
 const { OpenAIApi, Configuration } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: "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"
-});
-
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
 module.exports = openai;
